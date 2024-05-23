d9948979f529b8b2214701eea72099316fccddf9
karenfatalla <167407924+karenfatalla@users.noreply.github.com>
2024-05-23 23:11:25 +0800

    Add files via upload

---
+++ database/sign up.sql
@@ -0,0 +1,50 @@
+
+Table structure for table web
+CREATE TABLE web (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+-------------------------------------------------------------------------------
+Table structure for table login
+
+CREATE TABLE `login` (
+  `user` varchar(10) NOT NULL,
+  `pass` varchar(10) NOT NULL,
+  `date_time` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-------------------------------------------------------------------------------
+Table structure for information
+
+CREATE TABLE `information` (
+  `pname` varchar(30) NOT NULL,
+  `pdescription` varchar(10000) NOT NULL,
+  `pi_main` varchar(1000) NOT NULL,
+  `pi1` varchar(1000) NOT NULL,
+  `pi2` varchar(1000) NOT NULL,
+  `pi3` varchar(1000) NOT NULL,
+  `package` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--------------------------------------------------------------------------------
+Table structure for booking
+
+CREATE TABLE `booking` (
+  `id` int(10) NOT NULL,
+  `ffirst` varchar(20) NOT NULL,
+  `flast` varchar(20) NOT NULL,
+  `femail` varchar(30) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `fphone` int(15) NOT NULL,
+  `fdesti` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-------------------------------------------------------------------------------
+Table structure for feedback
+
+CREATE TABLE `feedback` (
+  `id` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `feedbk` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
